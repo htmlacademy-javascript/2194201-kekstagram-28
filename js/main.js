68f83b0b131e5a6e16bbcf88d8a45679df61d1cf
NeXan <nexan87@gmail.com>
2023-03-01 09:05:30 +0300

    Рефакторит скрипт

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+const PHOTO_COUNT = 25;
+const MIN_LIKES = 15;
+const MAX_LIKES = 200;
+const MIN_COMMENTS = 1;
+const MAX_COMMENTS = 7;
+
 const NAMES = [
   'Алиса', 'Евгений', 'Елена', 'Роман', 'Светлана', 'Сергей', 'Юлия'
 ];
@@ -11,78 +17,41 @@ const COMMENTS = [
   'Лица у людей на фотке перекошены, как будто их избивают.Как можно было поймать такой неудачный момент ?!'
 ];
 
-const getRandomInteger = (min, max) => {
-  const lower = Math.ceil(Math.min(min, max));
-  const upper = Math.floor(Math.max(min, max));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
+const DESCRIPTIONS = [
+  'Классное фото!',
+  'Наша лучшая фотография!',
+  'Оцените ракурс, плиз...',
+  'Твое лицо, когда ты не умеешь фотографировать.',
+  'Это моя бывшая!!!',
+  'Купил новый фотоаппарат, оцените снимок.'
+];
+
+let photoId = 1;
+let commentId = 1;
+
+const getRandomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+const createMessage = () => {
+  const message = Array.from({ length: getRandomInteger(1, 2) }, () => getRandomArrayElement(COMMENTS));
+  return [...new Set(message)].join(' ');
 };
 
-const createRandomId = (min, max, noRepeat) => {
-  const previousValues = [];
+const createComment = () => ({
+  id: commentId++,
+  avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+  message: createMessage(),
+  name: getRandomArrayElement(NAMES),
+});
 
-  return function () {
-    let currentValue = getRandomInteger(min, max);
+const createPhoto = () => ({
+  id: photoId,
+  url: `photos/${photoId++}.jpg`,
+  description: getRandomArrayElement(DESCRIPTIONS),
+  likes: getRandomInteger(MIN_LIKES, MAX_LIKES),
+  comments: Array.from({ length: getRandomInteger(MIN_COMMENTS, MAX_COMMENTS) }, createComment),
+});
 
-    if (noRepeat) {
-      if (previousValues.length >= (max - min + 1)) {
-        return null;
-      }
-      while (previousValues.includes(currentValue)) {
-        currentValue = getRandomInteger(min, max);
-      }
-      previousValues.push(currentValue);
-    }
+const getPhotos = () => Array.from({ length: PHOTO_COUNT }, createPhoto);
 
-    return currentValue;
-  };
-};
-
-const createPhotoId = createRandomId(1, 25, true);
-const createCommentTotal = createRandomId(0, 2);
-const createCommentId = createRandomId(1, 1000, true);
-const createAuthor = createRandomId(0, NAMES.length - 1);
-const createAvatar = createRandomId(1, NAMES.length - 1);
-
-const stickMessage = () => {
-  const createMessage = createRandomId(0, COMMENTS.length - 1, true);
-
-  let message = `${COMMENTS[createMessage()]}`;
-
-  if (getRandomInteger(0, 1)) {
-    message += ` ${COMMENTS[createMessage()]}`;
-  }
-
-  return message;
-};
-
-const addComment = () => {
-  const commentArray = [];
-
-  for (let i = 0; i < createCommentTotal(); i++) {
-    commentArray.push({
-      id: createCommentId(),
-      avatar: `img/avatar-${createAvatar()}.svg`,
-      message: stickMessage(),
-      name: NAMES[createAuthor()],
-    });
-  }
-
-  return commentArray.length > 0 ? commentArray : null;
-};
-
-const createPhoto = () => {
-  const photoId = createPhotoId();
-
-  return ({
-    id: photoId,
-    url: `photos/${photoId}.jpg`,
-    description: `Описание к фото №${photoId}`,
-    likes: getRandomInteger(15, 200),
-    comments: addComment(),
-  });
-};
-
-const similarWizards = Array.from({ length: 25 }, createPhoto);
-
-console.log(similarWizards);
+getPhotos();
