1e89c82efa7bfb9a68c66d482f005e17d1d56bf8
NeXan <nexan87@gmail.com>
2023-03-14 15:11:27 +0300

    Завершает валидацию формы

--- js/form-validation.js
+++ js/form-validation.js
@@ -3,7 +3,9 @@ const hashTagField = document.querySelector('.text__hashtags');
 const descriptionField = document.querySelector('.text__description');
 const regExp = /^#[a-zа-яё0-9]{1,19}$/i;
 const MAX_HASH_TAGS = 5;
-const messageErrorHagTag = 'Не более 5 уникальных хэштэгов, длина каждого < 20 символов. Должен начинаться с # и состоять из букв или цифр!';
+const MAX_SYMBOL_DESCRIPTION = 140;
+const messageErrorHagTag = `Не более ${MAX_HASH_TAGS} уникальных хэштэгов < 20 символов каждый. Сначала #, а после - буквы и цифры!`;
+const messageErrorDescription = `Комментарий должен быть не более ${MAX_SYMBOL_DESCRIPTION} символов. Уменьшите количество символов!`;
 
 const pristine = new Pristine(editPhotoForm, {
   classTo: 'img-upload__field-wrapper',
@@ -12,7 +14,7 @@ const pristine = new Pristine(editPhotoForm, {
   errorTextParent: 'img-upload__field-wrapper',
   errorTextTag: 'span',
   errorTextClass: 'img-upload__error',
-});
+}, false);
 
 const checkHashTagRegExp = (hashTag) => !regExp.test(hashTag);
 
@@ -22,10 +24,12 @@ const checkHashTagsLength = (hashTags) => hashTags.length > MAX_HASH_TAGS;
 
 const validateHashTags = () => {
   const hashTags = hashTagField.value.trim().split(' ');
-  const isValid = hashTags.some((hashTag) => checkHashTagRegExp(hashTag) || checkHashTagSame(hashTags) || checkHashTagsLength(hashTags));
+  const isValid = hashTags.some((hashTag) => checkHashTagRegExp(hashTag)) || checkHashTagSame(hashTags) || checkHashTagsLength(hashTags);
 
   return hashTagField.value.length === 0 ? isValid : !isValid;
 };
+
+const validateDescription = () => descriptionField.value.length < MAX_SYMBOL_DESCRIPTION;
 
 function onEditPhotoFormSubmit(evt) {
   if (!pristine.validate()) {
@@ -34,5 +38,6 @@ function onEditPhotoFormSubmit(evt) {
 }
 
 pristine.addValidator(hashTagField, validateHashTags, messageErrorHagTag);
+pristine.addValidator(descriptionField, validateDescription, messageErrorDescription);
 
 editPhotoForm.addEventListener('submit', onEditPhotoFormSubmit);
