b2d8dfc6124458ac470bee4a2fd4a7b463379e32
NeXan <nexan87@gmail.com>
2023-03-14 13:19:30 +0300

    Пишет код валидации формы

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,64 +1,48 @@
 const editPhotoForm = document.querySelector('.img-upload__form');
 const hashTagField = document.querySelector('.text__hashtags');
 const descriptionField = document.querySelector('.text__description');
-// const hashTag = /^#[a-zа-яё0-9]{1,19}$/i;
-const pristine = new Pristine(editPhotoForm);
-
+const regExp = /^#[a-zа-яё0-9]{1,19}$/i;
+const pristine = new Pristine(editPhotoForm, {}, false);
 const MAX_HASH_TAGS = 5;
 
-const checkHashTag = [
-  {
-    regExp: /^#[a-zа-яё0-9]{1,19}$/i,
-    message: 'Хэштег должен начинаться с # и состоять только из букв и чисел, а также иметь длину не более 20 символов',
-    check: function (hashTags, hashTag) {
-      return !this.regExp.test(hashTag);
-    },
-  },
-  {
-    message: 'Одинаковые хэштэги недопустимы',
-    check: (hashTags, hashTag) => {
+const setMessageErrorHashTags = (message) => hashTagField.setCustomValidity(message);
 
-      for (let i = 0; i < hashTags.length; i++) {
-        for (let j = i + 1; j < hashTags.length; j++) {
-          if (hashTags[i].toLowerCase() === hashTags[j].toLowerCase()) {
-            return true;
-          }
-        }
-      }
+const checkHashTagRegExp = (hashTag) => {
+  const checkHashTag = !regExp.test(hashTag);
+  if (checkHashTag) {
+    setMessageErrorHashTags('Хэштег начинается с #, состоит из букв и чисел, длина < 20 символов');
+  }
+  return checkHashTag;
+};
 
-      return false;
-    }
-  },
-  {
-    message: 'Разрешено добавлять не более 5 хэштэгов',
-    check: (hashTags, hashTag) => hashTags.length > MAX_HASH_TAGS,
-  },
-];
+const checkHashTagSame = (hashTags) => {
+  const checkSame = hashTags.some((item, index) => hashTags.indexOf(item.toLowerCase()) !== index);
+  if (checkSame) {
+    setMessageErrorHashTags('Одинаковые хэштэги недопустимы');
+  }
+  return checkSame;
+};
 
-const getHashTagsErrorMessage = (index) => {
-
-  const message = checkHashTag[index].message;
-  console.log(message);
-  return message;
+const checkHashTagsLength = (hashTags) => {
+  const checkLength = hashTags.length > MAX_HASH_TAGS;
+  if (checkLength) {
+    setMessageErrorHashTags('Разрешено добавлять не более 5 хэштэгов');
+  }
+  return checkLength;
 };
 
 const validateHashTags = () => {
   const hashTags = hashTagField.value.trim().split(' ');
+  const isValid = hashTags.some((hashTag) => checkHashTagRegExp(hashTag) || checkHashTagSame(hashTags) || checkHashTagsLength(hashTags));
 
-  hashTags.forEach((hashTag) => {
-
-    checkHashTag.forEach((object, index) => {
-      if (checkHashTag[index].check(hashTags, hashTag)) {
-        return getHashTagsErrorMessage(index);
-      }
-    });
-
-  });
+  return hashTagField.value.length === 0 ? isValid : !isValid;
 };
 
 function onEditPhotoFormSubmit(evt) {
-  evt.preventDefault();
-  // pristine.validate();
+  if (!pristine.validate()) {
+    evt.preventDefault();
+    hashTagField.reportValidity();
+  }
 }
 
 pristine.addValidator(hashTagField, validateHashTags);
