2f6e7c38954ac9e3dfd548307f5da06749e395df
NeXan <nexan87@gmail.com>
2023-03-28 08:42:40 +0300

    Изменяет название функции

--- js/utils.js
+++ js/utils.js
@@ -13,13 +13,13 @@ export const debounce = (callback, timeoutDelay) => {
   };
 };
 
-export const renderMessage = (element) => document.body.append(element);
+export const renderErrorElement = (element) => document.body.append(element);
 
 export const createErrorElement = (message) => {
   const div = document.createElement('div');
   div.classList.add('error-message');
   div.textContent = message;
-  renderMessage(div);
+  renderErrorElement(div);
 };
 
 export const removeErrorElement = () => {
