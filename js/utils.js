082a81af030df2dcc9f324ddeafca51d49c68c58
NeXan <nexan87@gmail.com>
2023-03-29 11:27:27 +0300

    Удаляет функции в utils

--- js/utils.js
+++ js/utils.js
@@ -1,7 +1,3 @@
-const getRandomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
-
-const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
-
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
 const debounce = (callback, timeoutDelay) => {
@@ -27,4 +23,4 @@ const removeErrorElement = () => {
   errorMessage.remove();
 };
 
-export { getRandomInteger, getRandomArrayElement, isEscapeKey, debounce, renderElement, createErrorElement, removeErrorElement };
+export { isEscapeKey, debounce, renderElement, createErrorElement, removeErrorElement };
